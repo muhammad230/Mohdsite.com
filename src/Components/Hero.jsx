fd8cecd38dc4684a288d381b62163af63e48b2e1
muhammad230 <ali.khansmr3@gmail.com>
2026-03-03 14:01:30 +0500

    Add a button to Hero/section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,6 +13,18 @@ const Hero = () => {
         <p className="text-gray-300 mb-8">
         Crafting digital experiences that blend innovation with aesthetics
         </p>
+        <div className="flex gap-4">
+            <button className="px-6 py-3 rounded-full 
+                             bg-linear-to-br from-pink-500 to-purple-600 
+                             hover:scale-105 transition duration-300">
+            View Projects
+          </button>
+          <button className="px-6 py-3 rounded-full 
+                             border border-white/30 
+                             hover:bg-white/10 transition duration-300">
+            Let's Talk
+          </button>
+        </div>
       </div>
     </section>
   );
