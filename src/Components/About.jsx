770fbcb5bf239f146ec42e38ced7ad29248f246b
muhammad230 <ali.khansmr3@gmail.com>
2026-03-04 14:30:33 +0500

    add paragraph and h2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-    </div>
+   <section id='about' className="bg-[#140b2d] text-white py-24 px-6">
+     <div className="max-w-6xl mx-auto">
+      <p className='text-center text-purple-400 tracking-widest mb-4'>
+        ABOUT ME
+      </p>
+      <h2 className="text-center text-5xl md:text-6xl font-bold mb-16">
+          Passion Meets{" "}
+          <span className="bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
+            Precision
+          </span>
+        </h2>
+     </div>
+   </section>   
   )
 }
 
