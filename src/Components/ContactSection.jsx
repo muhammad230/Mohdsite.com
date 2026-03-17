1e09610a5e72d8c5eb5a146936d1644325b4b2a8
muhammad230 <ali.khansmr3@gmail.com>
2026-03-17 19:52:48 +0500

    add p tag and h2 tag

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const ContactSection = () => {
   return (
-    <div>
-      <h1>contact section here
-        
-      </h1>
-    </div>
-  )
-}
+    <section className="bg-[#03000a] py-20 px-6 text-white">
+      <div className="text-center mb-16">
+        <p className="text-purple-400 text-xl  mb-4">
+          Get In Touch
+        </p>
+         <h2 className="text-6xl font-bold mb-4">
+          Let's Create{" "}
+          <span className="bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text text-transparent">
+            Together
+          </span>
+        </h2>
+      </div>
+    </section>
+  );
+};
 
-export default ContactSection
+export default ContactSection;
