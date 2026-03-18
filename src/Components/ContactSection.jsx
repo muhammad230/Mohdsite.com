a22e16d1260e385df52720a4bed80a5bc0a5e5f1
muhammad230 <ali.khansmr3@gmail.com>
2026-03-18 13:08:32 +0500

    add more data and add icons

--- src/Components/ContactSection.jsx
+++ src/Components/ContactSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaEnvelope, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const ContactSection = () => {
   return (
@@ -19,19 +20,55 @@ const ContactSection = () => {
       </div>
 
       <div className="max-w-6xl mx-auto grid ms:grid-col-2 gap-12">
-        {/* left */}   
+        {/* left */}
         <div>
-            <div className="flex items-center gap-4 mb-8 
-transition-all duration-300 hover:translate-x-3 cursor-pointer">
-                <div className="p-4 rounded-xl bg-gradient-to-r from-purple-400 to-pink-500">
+          {/* email */}
+          <div
+            className="flex items-center gap-4 mb-8 
+transition-all duration-300 hover:translate-x-3 cursor-pointer"
+          >
+            <div className="p-4 rounded-xl bg-gradient-to-r from-purple-400 to-pink-500">
               <FaEnvelope />
             </div>
             <div>
               <p className="text-gray-400">Email</p>
-              <h3 className="text-white text-semibold">hello@developer.com</h3>
+              <h3 className="text-white text-semibold">
+                ali.khansmr3@gmail.com
+              </h3>
             </div>
-
+          </div>
+          {/* phone */}
+          <div className="flex items-center gap-4 mb-8 transition-all duration-300 hover:translate-x-3 cursor-pointer">
+            <div className="p-4 rounded-xl bg-gradient-to-r from-purple-500 to-pink-500">
+              <FaPhone />
             </div>
+            <div>
+              <p className="text-gray-400">Phone</p>
+              <h3 className="font-semibold">+92 1618 64554</h3>
+            </div>
+          </div>
+          {/* location */}
+          <div className="flex items-center gap-4 mb-8 transition-all duration-300 hover:translate-x-3 cursor-pointer">
+            <div className="p-4 rounded-xl bg-gradient-to-r from-purple-500 to-pink-500">
+              <FaMapMarkerAlt />
+            </div>
+            <div>
+              <p className="text-gray-400">Location</p>
+              <h3 className="font-semibold">Gilgit, Pakistan</h3>
+            </div>
+          </div>
+          <h3 className="mt-10 mb-4 font-semibold">Connect With Me</h3>
+          <div className="flex gap-4">
+            <span className="p-3 rounded-full border border-gray-700 hover:bg-purple-500 transition cursor-pointer hover-rubber">
+              <FaGithub />
+            </span>
+            <span className="p-3 rounded-full border border-gray-700 hover:bg-purple-500 transition cursor-pointer hover-rubber">
+              <FaLinkedin />
+            </span>
+            <span className="p-3 rounded-full border border-gray-700 hover:bg-purple-500 transition cursor-pointer hover-rubber">
+              <FaTwitter />
+            </span>
+          </div>
         </div>
       </div>
     </section>
